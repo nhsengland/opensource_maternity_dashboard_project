4f5bfc4282b0306d6f2473f99dce4c357475b968
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-05-02 12:55:40 +0100

    updated folder path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash import Dash, html
 app = Dash(__name__)
 
 # Get the list of PNG file paths in the 'prelim-charts' folder
-image_folder = 'prelim_charts'
+image_folder = 'prelim_charts/ALL SUBMITTERS'
 image_paths = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith('.png')]
 
 # Encode the PNG images as base64 strings
