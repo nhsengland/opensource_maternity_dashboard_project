072e95d2d417f5cd6cef5a6372e0c33b472fb2c1
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-05-02 14:37:56 +0100

    add charts for each org

--- prelim_data_investigation.py
+++ prelim_data_investigation.py
@@ -2,9 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-#import data
-df = pd.read_csv("hosp-epis-stat-mat-msdscsv-2022-23.csv")
-
 def create_bar_chart(dimension, org_name):
     """
     Create a bar chart for each dimension to figure out what the data looks like.
@@ -29,7 +26,7 @@ def create_bar_chart(dimension, org_name):
     ax.set_xticklabels(label_lines, rotation=0, ha='center')
     
     # Set chart title and y-axis label
-    plt.title(dimension + " at National level")
+    plt.title(dimension + f" for {org_name}")
     plt.ylabel("Count for 2022-23 year")
     
     # Adjust the bottom margin to prevent labels from being cut off
@@ -43,12 +40,22 @@ def create_bar_chart(dimension, org_name):
     plt.savefig(os.path.join(folder_path, f"{dimension}.png"), bbox_inches='tight')
     plt.close()
 
-
+df = pd.read_csv("hosp-epis-stat-mat-msdscsv-2022-23.csv")
 # Replace missing values in "Measure" with values from "Dimension" - This is just for Total where there is no measure for Total
 df['Measure'] = df['Measure'].fillna(df['Dimension'].map(str))
 
+"""
 # Create bar charts for each Dimension
-for dimension in df["Dimension"].unique():
-    create_bar_chart(dimension, "ALL SUBMITTERS")
+# Run this to recreate charts for ALL orgs.
+# Will take at least 10 minutes
+for org in df["Org_Name"].unique():
+    for dimension in df["Dimension"].unique():
+        create_bar_chart(dimension, org)
+"""
+# Filter the DataFrame to include only rows where Org_Level is "Provider"
+provider_df = df[df['Org_Level'] == 'Provider']
 
-print(df["Org_Name"].unique())
+# Count the number of unique values in the Org_Name column
+num_unique_providers = provider_df['Org_Name'].nunique()
+
+print(f"Number of unique providers: {num_unique_providers}")
