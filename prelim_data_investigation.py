cacd0d1d3fc3605e5a6ca7a2b8b8bb14d88c7ae0
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-05-02 12:56:15 +0100

    updated to include multiple org levels

--- prelim_data_investigation.py
+++ prelim_data_investigation.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 #import data
 df = pd.read_csv("hosp-epis-stat-mat-msdscsv-2022-23.csv")
 
-def create_bar_chart(dimension):
+def create_bar_chart(dimension, org_name):
     """
     Create a bar chart for each dimension to figure out what the data looks like.
     Caveat: just using the national data for now
     """
     # filter to national
-    df_national = df[df["Org_Level"] == "National"]
+    df_national = df[df["Org_Name"] == org_name]
     
     # filter to just the dimension we want
     df_filtered = df_national[df_national["Dimension"] == dimension]
@@ -34,8 +35,12 @@ def create_bar_chart(dimension):
     # Adjust the bottom margin to prevent labels from being cut off
     plt.subplots_adjust(bottom=0.5)
     
-    # Save the chart
-    plt.savefig(f"prelim_charts/{dimension}", bbox_inches='tight')
+    # Create the folder path
+    folder_path = os.path.join('prelim_charts', org_name)
+    # Create the folder if it doesn't exist
+    os.makedirs(folder_path, exist_ok=True)
+    # Save the figure
+    plt.savefig(os.path.join(folder_path, f"{dimension}.png"), bbox_inches='tight')
     plt.close()
 
 
@@ -44,4 +49,6 @@ df['Measure'] = df['Measure'].fillna(df['Dimension'].map(str))
 
 # Create bar charts for each Dimension
 for dimension in df["Dimension"].unique():
-    create_bar_chart(dimension)
+    create_bar_chart(dimension, "ALL SUBMITTERS")
+
+print(df["Org_Name"].unique())
