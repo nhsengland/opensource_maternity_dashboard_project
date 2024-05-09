91c2e612dcbb508f290389ff400a82ceab8fd422
Alex Tibbs <alex.tibbs@cabinetoffice.gov.uk>
2024-05-09 16:47:11 +0100

    Fix the map

--- plotly_map.py
+++ plotly_map.py
@@ -1,29 +1,20 @@
 import thin_slice_tb_region_map
 import plotly.express as px
-import dash
-from dash import dcc, html
-import pandas as pd
-import json
 
 # Create choropleth map
 
 # Load DataFrame
 df = thin_slice_tb_region_map.return_data_for_map()
-
-# Load GeoJSON file
-with open('NHS_England_Regions_April_2021_EN_BUC_2022.geojson', 'r') as f:
-    geojson_data = json.load(f)
 df = df.to_crs(epsg='4326')
 
-
 # Create choropleth map
+geo = df[["region_name", "geometry"]].set_index("region_name")
 fig = px.choropleth_mapbox(df, 
-                            geojson=df.geometry, 
+                            geojson=geo, 
                             locations="region_name", 
-                            #featureidkey="properties.NHSER21NM",
                             color='births per 1000',
                             mapbox_style="carto-positron",
                             center={"lat": 50, "lon": 0},
                             zoom=5)
 
-fig.write_html("map.html")
+fig.write_html("output/map.html")
