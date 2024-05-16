b68d546637897e293eea61cd26294f5e926c2694
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-05-16 09:34:57 +0100

    updated colours

--- plotly_map.py
+++ plotly_map.py
@@ -1,8 +1,6 @@
 import thin_slice_tb_region_map
 import plotly.express as px
 
-# Create choropleth map
-
 # Load DataFrame
 df = thin_slice_tb_region_map.return_data_for_map()
 # We need to switch to latitude-longitude, geopandas has a useful to_crs method for this
@@ -14,10 +12,15 @@ df = df.to_crs(epsg='4326')
 # This is a bit odd as we have to supply the data twice with different arguments, so there's probably a neater
 # way to do it!
 geo = df[["region_name", "geometry"]].set_index("region_name")
+
+# Define NHS colors
+nhs_colors = ['#B4D0FF', '#699EFF', '#1E6EFF', '#003087', '#001843']
+
 fig = px.choropleth_mapbox(df, 
                             geojson=geo, 
                             locations="region_name", 
                             color='births per 1000',
+                            color_continuous_scale=nhs_colors,
                             mapbox_style="carto-positron",
                             center={"lat": 50, "lon": 0},
                             zoom=5)
