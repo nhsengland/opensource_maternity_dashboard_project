686702191a0ff1ea6ea1c254e152959296c87615
Jennifer Struthers <jennifer.struthers1@nhs.net>
2024-06-03 14:27:44 +0100

    updated layout

--- create_maps/app.py
+++ create_maps/app.py
@@ -1,10 +1,10 @@
 from dash import Dash, dcc, html, Input, Output, callback
-import json
 import process_data
 import plotly.express as px
 import sys
 import geopandas as gpd
 import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 sys.path.append('./')
 import config
 
@@ -27,7 +27,7 @@ def get_map(org_level, dimension):
                                 color=config.measure_dict[dimension]["rate_col"],
                                 color_continuous_scale=nhs_colors,
                                 mapbox_style="carto-positron",
-                                center={"lat": 53, "lon": 0},
+                                center={"lat": 53, "lon": -2},
                                 zoom=5)
 
 
@@ -36,9 +36,32 @@ def get_map(org_level, dimension):
     return fig
 
 def get_bar_chart(org_level, dimension, location):
-    df = process_data.return_data_for_bar_chart(dimension, org_level, location)
-    fig = px.bar(df, x= "Measure", y="Value", title=location)
+    df_location = process_data.return_data_for_bar_chart(dimension, org_level, location)
+    df_all_submitters = process_data.return_data_for_bar_chart(dimension, "National", "All Submitters")
+
+    # Merge together the df with the All Submitters data to get marker data
+    df_merged = process_data.merge_total_submitters(df_location, df_all_submitters)
+
+    # Create the bar chart
+    fig = px.bar(df_merged, x="Measure", y="Value", title=f"{location}: {dimension}. Bar chart of broken down data, with markers comparing to All Submitters")
+    
+    # Add custom markers for All Submitters
+    fig.add_trace(
+        go.Scatter(
+            x=df_merged['Measure'], 
+            y=df_merged['All Submitters Value'], 
+            mode='markers',
+            name='All Submitters',
+            marker=dict(
+                symbol='cross',
+                size=10,
+                color='red'
+            )
+        )
+    )
+    
     return fig
+
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
@@ -46,7 +69,7 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "23rem",
+    "width": "20rem",  # Adjusted the width to reduce the gap
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
@@ -54,7 +77,7 @@ SIDEBAR_STYLE = {
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "25rem",
+    "margin-left": "8rem", 
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
@@ -72,24 +95,32 @@ sidebar = html.Div(
 )
 
 content = html.Div(
-    [
-        dcc.Graph(
-            id='map',
-            figure=get_map(org_level, dimension)
-        ),
-        dcc.Graph(
-            id='bar-chart',
-            figure=get_bar_chart(org_level, dimension, location="London")
-        )
-    ], 
+    dbc.Row(
+        [
+            dbc.Col(
+                dcc.Graph(
+                    id='map',
+                    figure=get_map(org_level, dimension),
+                    style={"height": "800px"}
+                ), width=5, style={"padding": "0"}  # Removed padding from the column
+            ),
+            dbc.Col(
+                dcc.Graph(
+                    id='bar-chart',
+                    figure=get_bar_chart(org_level, dimension, location="All Submitters"),
+                    style={"height": "800px"}
+                ), width=7, style={"padding": "0"}  # Removed padding from the column
+            )
+        ]
+    ),
     style=CONTENT_STYLE
 )
 
 app.layout = html.Div([
     dcc.Location(id="url"),
     dbc.Row([
-        dbc.Col(sidebar, width=3),
-        dbc.Col(content, width=12)
+        dbc.Col(sidebar, width=1),
+        dbc.Col(content, width=11, style={"padding": "0"})  # Removed padding from the column
     ])
 ])
 
